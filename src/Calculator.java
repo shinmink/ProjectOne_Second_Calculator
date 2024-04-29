d245d4d3add71682832fbd551d672269f121feae
shinmink <sinmk99@hansung.ac.kr>
2024-04-29 10:46:02 +0900

    3. App 클래스의 main 메서드에서 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정합니다. (캡슐화)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,15 +1,27 @@
 import java.util.Scanner;
 
 public class Calculator {
+
     /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+    /* 연산 결과를 저장하는 컬렉션 타입 필드를 외부에서 직접 접근 하지 못하도록 수정*/
+    private static int total = 0;
+
+    public Calculator(){}; // 기본 생성자
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+
     class CalculatorException extends Exception {
         public CalculatorException(String message) {
             super(message);
         }
     }
-
-
-
 
     public int calculate(int Number1, int Number2, char operator) throws CalculatorException {
 
@@ -27,7 +39,6 @@ public class Calculator {
 //        - 입력받은 연산 기호를 구분하기 위해 제어문을 사용합니다. (e.g.if, switch)
 //        - 연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력합니다.
 //        - e.g. “나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. “
-
 
         /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
         if (Number1 <= 0 && Number2 <= 0) {
@@ -58,8 +69,8 @@ public class Calculator {
                     //continue;
             }
         }
-
-        System.out.println("결과: " + answer);
+        setTotal(answer);
+        System.out.println("결과: " + getTotal());
 
         // 5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.
         // 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다
@@ -85,6 +96,11 @@ public class Calculator {
 
 
         return answer;
+
+
     }
+
+
+
 
 }
